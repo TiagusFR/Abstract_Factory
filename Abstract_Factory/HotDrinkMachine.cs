f3f3cc6b7886106562eac309158c8e03c8364b77
Tiago França <t.iago_196@hotmail.com>
2024-08-27 22:11:16 -0300

    Add project files.

---
+++ Abstract_Factory/HotDrinkMachine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    public class HotDrinkMachine
+    {
+        private Dictionary<string, IHotDrinkFactory> factories = new Dictionary<string, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            factories.Add("tea", new TeaFactory());
+            factories.Add("coffee", new CoffeeFactory());
+        }
+
+        public IHotDrink MakeDrink(string type, int amount)
+        {
+            if (factories.ContainsKey(type))
+            {
+                return factories[type].Prepare(amount);
+            }
+            else
+            {
+                throw new ArgumentException("Drink type not supported");
+            }
+        }
+    }
+}
---
+++ Abstract_Factory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var machine = new HotDrinkMachine();
+
+            var tea = machine.MakeDrink("tea", 200);
+            tea.Consume();
+
+            var coffee = machine.MakeDrink("coffee", 50);
+            coffee.Consume();
+        }
+    }
+}
